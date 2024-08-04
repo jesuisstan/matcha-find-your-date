4a38bccd036ffe36af29da8d0753f7f48ecbac24
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-08-04 02:57:15 +0200

    working......

--- next.config.js
+++ next.config.js
@@ -36,18 +36,6 @@ const nextConfig = nextTranslate({
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://eu.i.posthog.com/:path*',
-      },
-    ];
-  },
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
 });
