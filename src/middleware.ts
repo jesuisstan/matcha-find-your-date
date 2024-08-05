4d32e0a7a25fa088d6a8ca25aeb535452e944fc0
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-08-05 03:53:24 +0200

    fix lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+
 import { jwtVerify } from 'jose';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
