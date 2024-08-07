e5e4f7287f484edabe4b7adb96764a724296b9dd
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-08-07 17:20:59 +0200

    new translation fixes...

--- i18n.ts
+++ i18n.ts
@@ -9,6 +9,6 @@ export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`../locales/${locale}.json`)).default,
+    messages: (await import(`./locales/${locale}.json`)).default,
   };
 });
