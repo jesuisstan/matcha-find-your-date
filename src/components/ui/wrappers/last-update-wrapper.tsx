04d4e6195ecd962df1b1869cb72f87b5707a9e83
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-08-13 18:48:40 +0200

    del utils from profile

--- src/components/ui/wrappers/last-update-wrapper.tsx
+++ src/components/ui/wrappers/last-update-wrapper.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { DateSkeleton } from '@/components/ui/wrappers/labels-wrapper';
-import { formatApiDateLastUpdate } from '@/utils/format-api-date';
 
 interface LastUpdateProps {
   loading: boolean;
@@ -13,7 +12,7 @@ const LastUpdateWrapper: React.FC<LastUpdateProps> = ({ loading, date }) => {
   return (
     <div className="mt-4 flex flex-row justify-end gap-1 text-right text-sm text-secondary">
       {/*{t`common:table-overview.last-update`}{' '}*/}
-      {loading || !date ? <DateSkeleton /> : formatApiDateLastUpdate(date ?? '')}
+      {loading || !date ? <DateSkeleton /> : (date ?? '')}
     </div>
   );
 };
