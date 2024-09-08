24e86cd023f372d38d8ac3e07a3a0a69e486134c
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-09-08 04:12:40 +0200

    workflow workout

--- src/app/api/check-inactivity/route.ts
+++ src/app/api/check-inactivity/route.ts
@@ -3,12 +3,14 @@ import { NextResponse } from 'next/server';
 import { db } from '@vercel/postgres';
 
 // the inactivity period (e.g., 10 minutes)
-const INACTIVITY_PERIOD_MINUTES = 10;
+const INACTIVITY_PERIOD_MINUTES = 1; // todo change to 10
 
 export async function GET() {
   const client = await db.connect();
 
   try {
+    const result = await client.query(`SELECT * FROM users WHERE online = true`); // debug
+
     // SQL query to update users' online status based on inactivity
     const updateQuery = `
       UPDATE users
@@ -20,7 +22,7 @@ export async function GET() {
     await client.query(updateQuery);
 
     return NextResponse.json({
-      message: `Inactivity check completed. Users inactive for more than ${INACTIVITY_PERIOD_MINUTES} minutes are now offline.`,
+      message: `Inactivity check completed. Users (${result}) inactive for more than ${INACTIVITY_PERIOD_MINUTES} minutes are now offline.`,
     });
   } catch (error) {
     console.error('Error updating user status:', error);
