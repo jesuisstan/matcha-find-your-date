ed7899d4561242a161e68762afbb722fd4bde096
Stan Krivtsoff <stan.krivtsov@gmail.com>
2024-10-15 00:36:45 +0200

    cron job updated

--- src/app/api/check-inactivity/route.ts
+++ src/app/api/check-inactivity/route.ts
@@ -2,21 +2,21 @@ import { NextResponse } from 'next/server';
 
 import { db } from '@vercel/postgres';
 
-const INACTIVITY_PERIOD_MINUTES = 10;
+const INACTIVITY_PERIOD_MINUTES = 15;
 
 export async function GET() {
   const client = await db.connect();
 
   try {
-    // Fetch users who are currently online
-    const onlineUsersResult = await client.query(`SELECT * FROM users WHERE online = true`);
+    //// Fetch users who are currently online
+    //const onlineUsersResult = await client.query(`SELECT * FROM users WHERE online = true`);
 
-    // Fetch users who have been inactive for more than INACTIVITY_PERIOD_MINUTES
-    const inactiveUsersResult = await client.query(`
-      SELECT * FROM users
-      WHERE last_action < NOW() - INTERVAL '${INACTIVITY_PERIOD_MINUTES} minutes'
-      AND online = true;
-    `);
+    //// Fetch users who have been inactive for more than INACTIVITY_PERIOD_MINUTES
+    //const inactiveUsersResult = await client.query(`
+    //  SELECT * FROM users
+    //  WHERE last_action < NOW() - INTERVAL '${INACTIVITY_PERIOD_MINUTES} minutes'
+    //  AND online = true;
+    //`);
 
     // SQL query to update users' online status based on inactivity
     const updateQuery = `
@@ -29,8 +29,8 @@ export async function GET() {
 
     return NextResponse.json({
       message: `Inactivity check completed. Users inactive for more than ${INACTIVITY_PERIOD_MINUTES} minutes are now offline.`,
-      onlineUsers: onlineUsersResult.rows,      // Currently online users before the check
-      inactiveUsers: inactiveUsersResult.rows,  // Users marked as inactive
+      //onlineUsers: onlineUsersResult.rows, // Currently online users before the check
+      //inactiveUsers: inactiveUsersResult.rows, // Users marked as inactive
     });
   } catch (error) {
     console.error('Error updating user status:', error);
