cf8a07ba17e7d00683274b2e2cf54ca3512e964f
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-12-26 02:21:28 +0100

    slide-show added to carousel

--- src/components/carousel/embla-carousel.tsx
+++ src/components/carousel/embla-carousel.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 import { EmblaOptionsType } from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
 import useEmblaCarousel from 'embla-carousel-react';
+import { CirclePlay, CircleStop } from 'lucide-react';
 
 import {
   NextButton,
@@ -9,6 +12,8 @@ import {
   usePrevNextButtons,
 } from '@/components/carousel/embla-carousel-arrow-buttons';
 import { DotButton, useDotButton } from '@/components/carousel/embla-carousel-dot-button';
+import { useAutoplay } from '@/components/carousel/embla-carousel-use-autoplay';
+import { ButtonMatcha } from '@/components/ui/button-matcha';
 
 type TPropType = {
   slides: React.ReactNode[];
@@ -16,21 +21,23 @@ type TPropType = {
 };
 
 const EmblaCarousel: React.FC<TPropType> = (props) => {
+    const t = useTranslations();
+  
   const { slides, options } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options);
-  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    Autoplay({ playOnInit: true, delay: 3000 }),
+  ]);
   const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } =
     usePrevNextButtons(emblaApi);
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
+  const { autoplayIsPlaying, toggleAutoplay, onAutoplayButtonClick } = useAutoplay(emblaApi);
 
   return (
     <section className="mx-auto max-w-[48rem]">
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex touch-pan-y touch-pinch-zoom">
           {slides.map((slide, index) => (
-            <div key={index}>
-              {/*<div className="min-w-0 flex-none basis-[80%]" key={index}>*/}
-              {slide}
-            </div>
+            <div key={index}>{slide}</div>
           ))}
         </div>
       </div>
@@ -50,6 +57,14 @@ const EmblaCarousel: React.FC<TPropType> = (props) => {
             />
           ))}
         </div>
+      </div>
+      <div className="flex items-center justify-center self-center">
+        <ButtonMatcha onClick={toggleAutoplay} type="button" variant={'ghost'} size={'default'}>
+          <div className="flex items-center justify-center gap-2">
+            {autoplayIsPlaying ? <CircleStop size={30} /> : <CirclePlay size={30} />}
+            <p className="text-sm">{t('slide-show')}</p>
+          </div>
+        </ButtonMatcha>
       </div>
     </section>
   );
