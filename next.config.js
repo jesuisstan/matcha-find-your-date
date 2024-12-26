dab1bdc4e607d32699bd4805479104a72be5ee94
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-12-26 02:33:09 +0100

    urls for imgs added to next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,24 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = withNextIntl({
   reactStrictMode: false, // Disable React Strict Modecd
   images: {
-    domains: ['kdbxq6eseiqtwhzx.public.blob.vercel-storage.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kdbxq6eseiqtwhzx.public.blob.vercel-storage.com', // Vercel's blob URL
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com', // GitHub's avatar URL
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.intra.42.fr', // 42's avatar URL
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Google's avatar URL
+      },
+    ],
   },
   async redirects() {
     return [
